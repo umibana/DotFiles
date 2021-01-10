3d297490459289adafb1e120e6da0c20c3591752
hakuya <hans.villarroel@usach.cl>
2021-01-09 23:28:35 -0300

    Added initial files

---
+++ .config/vivaldi/homepage/bookmarks.js
@@ -0,0 +1,47 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+      {
+        name: "Amazon Prime",
+        url: "https://www.amazon.com/Amazon-Video",
+      },
+    ],
+  },
+  {
+    title: "Internet Forums",
+    links: [
+      { name: "Technology", url: "https://boards.4channel.org/g/" },
+      {
+        name: "Otaku Culture",
+        url: "https://boards.4channel.org/jp/",
+      },
+      { name: "Fitness", url: "https://boards.4channel.org/fit/" },
+      {
+        name: "/r/Chile",
+        url: "https://reddit.com/r/Chile",
+      },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "/r/FeedTheBeast", url: "https://reddit.com/r/feedthebeast" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+];
