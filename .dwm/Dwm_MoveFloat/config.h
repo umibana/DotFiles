3cad60ce02cf07af092df49160a99298cf2ed4fc
hansonell <hans.villarroel@usach.cl>
2021-02-21 15:32:27 -0300

    added missing scripts

--- .dwm/Dwm_MoveFloat/config.h
+++ .dwm/Dwm_MoveFloat/config.h
@@ -16,8 +16,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = {"SF Pro Rounded:size=10",
-				 "JoyPixels:size=10" };
+static char *fonts[]          = {"SF Pro Rounded:size=10"};
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#e3e5e8";
