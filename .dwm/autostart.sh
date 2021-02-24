85402074fa8c552eb0b8cfe6bb1b4eec23e9cdfe
hansOnell <hans.villarroel@hotmail.cl>
2021-02-24 16:50:55 -0300

    readded dwm

---
+++ .dwm/autostart.sh
@@ -0,0 +1,14 @@
+# wasd with mayus + wasd
+xmodmap ~/.Xmodmap &
+#status bar
+dwmblocks &
+# Enables notification services
+dunst &
+# Restores wallpaper
+nitrogen --restore &
+# Faster Repeating
+xset r rate 300 50 &
+# Monitors
+~/.screenlayout/3Mon.sh &
+
+
---
+++ .dwm/dwmblocks/config.h
@@ -0,0 +1,42 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"⌨", "sb-kbselect", 0, 30}, */
+	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+/*	{"",	"sb-tasks",	10,	26},*/
+	{"",	"sb-music",	1,	11},
+/*	{"",	"sb-pacpackages",	0,	8},*/
+/*	{"",	"sb-news",		0,	6},*/
+/*	 {"",	"sb-crypto",	0,	13},*/ 
+    	{"",	"playing-now",	1,	12}, 
+	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
+	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
+	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"sb-price btc \"Bitcoin\" 💰",				9000,	21}, */
+/*	{"",	"sb-torrent",	20,	7},*/
+	{"",	"sb-cpubars",		10,	18},
+	{"",	"sb-cpu",		10,	18},
+	/* {"",	"sb-moonphase",	18000,	17}, */
+/*	{"",	"sb-forecast",	18000,	5},*/
+/*	{"",	"sb-mailbox",	180,	12},*/
+/*	{"",	"sb-nettraf",	1,	16},*/
+
+	{"",	"sb-memory",	10,	14},
+	{"",	"battery",	5,	3},
+	{"",	"battery1",	5,	3},
+	{"",	"sb-brightness",	0,	5},
+	{"",	"sb-bluetooth",	10,	8},
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-internet",	5,	4},
+	{"",	"sb-clock",	60,	1},
+/*	{"",	"sb-help-icon",	0,	15},*/
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = " | ";
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
