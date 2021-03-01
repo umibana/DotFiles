765ab0f7fcde7de188012d6093fd53651ce538ee
hansonell <hans.villarroel@usach.cl>
2021-03-01 17:06:20 -0300

    ncmpcpp now in columns

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,21 @@
+/home/hakuya/Music/wowaka/Unhappy Refrain (B-SIDE)/01 - 裏表ラバーズ　reverse Edge mix.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain (B-SIDE)/02 - とおせんぼ　天変地異編.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain (B-SIDE)/03 - ローリンガール　風林火山編.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain (B-SIDE)/04 - 僕のサイノウ　dnb mix.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain (B-SIDE)/05 - 積み木の人形　rnb mix.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain (B-SIDE)/06 - ずれていく　postrock mix.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain (B-SIDE)/07 - ワールドエンド・ダンスホール　dexholic mix.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/01 - アンハッピーリフレイン.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/02 - ローリンガール.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/03 - 積み木の人形.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/04 - 僕のサイノウ.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/05 - 日常と地球の額縁.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/06 - テノヒラ.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/07 - とおせんぼ.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/08 - ラインアート.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/09 - 裏表ラバーズ.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/10 - グレーゾーンにて。.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/11 - ずれていく.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/12 - リバシブルドール.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/13 - ワールズエンド・ダンスホール.flac
+/home/hakuya/Music/wowaka/Unhappy Refrain/14 - プリズムキューブ.flac
