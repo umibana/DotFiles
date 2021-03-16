6cff2e1856f051d1e3e3ae1924f68c5d9b5b7870
hansonell <hans.villarroel@usach.cl>
2021-03-15 22:25:51 -0300

    ricing yet again time two

--- .dwm/dmenu/config.h
+++ .dwm/dmenu/config.h
@@ -1,30 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"berry:size=11",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
-};
+    "berry:size=11", "JoyPixels:pixelsize=8:antialias=true:autohint=true"};
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#e3e5e8", "#1c2023" },
-	[SchemeSel] = { "#1c2023", "#e3e5e8" },
-	[SchemeOut] = { "#000000", "#222222" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#c795ae", "#1c2023"},
+    [SchemeSel] = {"#1c2023", "#c795ae"},
+    [SchemeOut] = {"#000000", "#222222"},
 };
 static const unsigned int alphas[SchemeLast][2] = {
-	/*		fgalpha		bgalphga	*/
-	[SchemeNorm] = { fgalpha, bgalpha },
-	[SchemeSel] = { fgalpha, bgalpha },
-	[SchemeOut] = { fgalpha, bgalpha },
+    /*		fgalpha		bgalphga	*/
+    [SchemeNorm] = {fgalpha, bgalpha},
+    [SchemeSel] = {fgalpha, bgalpha},
+    [SchemeOut] = {fgalpha, bgalpha},
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
