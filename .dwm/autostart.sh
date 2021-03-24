01d78c3fcdeb11bbbaf62a18ed268927cb2a4273
hansOnell <hans.villarroel@hotmail.cl>
2021-03-24 17:24:27 -0300

    firefox css smaller

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,13 +1,5 @@
-# wasd with mayus + wasd
-xmodmap ~/.Xmodmap &
 #status bar
 dwmblocks &
-# Enables notification services
-dunst &
-# Faster Repeating
-xset r rate 300 50 &
 # Monitors
 ~/.screenlayout/3Mon.sh &
-# Restores wallpaper
-walchange &
 
